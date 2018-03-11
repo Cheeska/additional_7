5b0f93dd3cda1fbbaa652d2d4aa07c9336122269
Yana <YanaLSoltan@gmail.com>
2018-03-11 14:18:16 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,63 +1,56 @@
-
-const UNASSIGNED = 0;
-
 module.exports = function solveSudoku(matrix) {
-    return solvematrix(matrix);
-}   
 
-function solvematrix(matrix) {
-    for (var row = 0; row < 9; row++) {
-        for (var col = 0; col < 9; col++) {
-            if (matrix[row][col] == UNASSIGNED){
-                for (var number = 1; number <= 9; number++) {
-                    if (isAllowed(row, col, number)) {
-                        matrix[row][col] = number;
-                        if (solvematrix()) {
-                            return true;
-                        } else {
-                            matrix[row][col] = UNASSIGNED;
-                        }
-                    }
-                }
+    var matrixNew = [];
+
+        for (i=0; i<matrix.length; i++){
+            for (j=0; j<matrix.length;j++){
+                matrixNew.push(matrix[i][j]);
+            }
+
+        }
+
+
+    function check_candidate(num, row, col) {
+        for (var i = 0; i < 9; i++) {
+            var b_index = ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) * 9 + (Math.floor(col / 3) * 3) + (i % 3);
+            if (num == matrixNew[(row * 9) + i] ||
+                num == matrixNew[col + (i * 9)] ||
+                num == matrixNew[b_index]) {
                 return false;
             }
         }
+        return true;
     }
-    return true;
-}
 
-function containsInRow(row, number) {
-    for (var i = 0; i < 9; i++) {
-        if (matrix[row][i] == number) {
+    function get_candidate(index) {
+        if (index >= matrixNew.length) {
             return true;
-        } 
-    }
-    return false;
-}
-
-function containsInCol(col, number) {
-    for (var i = 0; i < 9; i++) {
-        if (matrix[i][col] == number) {
-            return true;
+        } else if (matrixNew[index] != 0) {
+            return get_candidate(index + 1);
         }
-    }
-    return false;
-}
 
-function containsInBox(row, col, number) {
-    var r = row - row % 3;
-    var c = col - col % 3;
-
-    for (i = r; i < r + 3; i++) {
-        for (j = c; j < c + 3; j++) {
-            if (matrix[i][j] == number) {
-                return true;
-            } 
+        for (var i = 1; i <= 9; i++) {
+            if (check_candidate(i, Math.floor(index / 9), index % 9)) {
+                matrixNew[index] = i;
+                if (get_candidate(index + 1)) {
+                    return true;
+                }
+            }
         }
-    }
-    return false;
-}
 
-function isAllowed(row, col, number) {
-    return !(containsInRow(row, number) || containsInCol(col, number) || containsInBox(row, col, number));
+        matrixNew[index] = 0;
+        return false;
+    }
+
+    function chunk_in_groups(arr) {
+        var result = [];
+        for (var i = 0; i < arr.length; i += 9) {
+            result.push(arr.slice(i, i + 9));
+        }
+        return result;
+    }
+
+    return !get_candidate(0) ? 'No solution found.' :
+        chunk_in_groups(matrixNew);
+
 }
